4cfa529bc1ff93bc58f767bdcad8d6142fd2be6d
Dalvinder Salinas Saldaña <dsalinas@bancosol.com.bo>
2025-11-21 19:27:10 -0400

    adding vm

--- 04-practica/main.tf
+++ 04-practica/main.tf
@@ -91,3 +91,46 @@ resource "azurerm_network_interface" "terraform_nic" {
     public_ip_address_id          = azurerm_public_ip.terraform_public_ip.id
   }
 }
+
+resource "azurerm_network_interface_security_group_association" "terraform_nic_nsg_association" {
+  network_interface_id      = azurerm_network_interface.terraform_nic.id
+  network_security_group_id = azurerm_network_security_group.terraform_nsg.id
+}
+
+resource "azurerm_virtual_machine" "main" {
+  name                  = "${var.prefix_name}-${var.environment}-vm"
+  location              = var.location
+  resource_group_name   = module.vnet_module_terraform.resource_group_name
+  network_interface_ids = [azurerm_network_interface.terraform_nic.id]
+  vm_size               = "Standard_DS1_v2"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  # delete_os_disk_on_termination = true
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  # delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "${var.prefix_name}-osdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "testadmin"
+    admin_password = "Dsalinas@dss!"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  # tags = {
+  #   environment = "staging"
+  # }
+}
