31bd0ed5aa5bfd3629e43cef2f372eda6d68a945
Dalvinder Salinas Saldaña <dsalinass@fassil.com.bo>
2025-11-26 21:04:15 -0400

    adding remote backend

---
+++ 09-practica/main.tf
@@ -0,0 +1,16 @@
+# Workflow
+# Write => Init => Plan => Apply => Destroy
+# terraform apply --auto-approve # Se utiliza para una confirmación automática de apply o destroy
+# terraform refresh # refrescar los cambios que se pudieron haber hecho directamente desde el portal
+# terraform plan -refresh-only # me muestra cambios manuales sin integrarlos al estado
+
+
+resource "azurerm_resource_group" "rg" {
+  name     = "dalvinder-rg"
+  location = "eastus"
+  tags = {
+    Project     = "Terraform"
+    Owner       = "Dalvinder"
+    Environment = "Dev"
+  }
+}
---
+++ 09-practica/state/locals.tf
@@ -0,0 +1,7 @@
+locals {
+  common_tags = {
+    environment = "dev"
+    owner       = "dalvinder"
+    project     = "terraform"
+  }
+}
